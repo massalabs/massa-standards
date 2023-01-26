c0e0926ab3819f4d62530023164c5725c63f970d
Nathan Seva <thykof@protonmail.ch>
2023-01-26 14:06:44 +0100

    Fic CD (#6)

--- smart-contracts/scripts/publish-dev.sh
+++ smart-contracts/scripts/publish-dev.sh
@@ -6,7 +6,7 @@ npm version --preid dev --no-git-tag-version --no-commit-hooks prepatch
 TIME=$(date -u +%Y%m%d%H%M%S)
 sed -i "/version/s/dev.0/dev.$TIME/g" package.json
 PUBLISH_VERSION=$(cat package.json | jq -r '.version')
-echo publishing @massalabs/massa-as-sdk@$PUBLISH_VERSION
+echo publishing @massalabs/sc-standards@$PUBLISH_VERSION
 
 # disable husky
 npm pkg delete scripts.prepare
