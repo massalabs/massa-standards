ac2494ad4184c04cdb7a261aa3cb32357cf7bf6a
Pierre Seznec <petterseznec@gmail.com>
2023-06-07 11:00:28 +0200

    refactor asserts for mint/burn

--- smart-contracts/assembly/contracts/FT/token-mint.ts
+++ smart-contracts/assembly/contracts/FT/token-mint.ts
@@ -28,14 +28,9 @@ export function mint(binaryArgs: StaticArray<u8>): void {
 
   assert(byteToBool(isOwner(Context.caller())), 'Caller is not the owner');
 
-  const isSuccessIncreaseTotalSupply = _increaseTotalSupply(amount);
-  assert(
-    isSuccessIncreaseTotalSupply,
-    'Requested burn amount causes an overflow',
-  );
+  _increaseTotalSupply(amount);
 
-  const isSuccessMint = _mint(recipient, amount);
-  assert(isSuccessMint, 'Requested burn amount causes an overflow');
+  _mint(recipient, amount);
 
   generateEvent(
     createEvent(MINT_EVENT_NAME, [recipient.toString(), amount.toString()]),
@@ -49,17 +44,17 @@ export function mint(binaryArgs: StaticArray<u8>): void {
  * @param amount -
  * @returns true if the token has been minted
  */
-function _mint(recipient: Address, amount: u64): boolean {
+function _mint(recipient: Address, amount: u64): void {
   const oldRecipientBalance = _balance(recipient);
   const newRecipientBalance = oldRecipientBalance + amount;
 
-  if (oldRecipientBalance > newRecipientBalance) {
-    return false;
-  }
+  // Check overflow
+  assert(
+    oldRecipientBalance < newRecipientBalance,
+    'Requested mint amount causes an overflow',
+  );
 
   _setBalance(recipient, newRecipientBalance);
-
-  return true;
 }
 
 /**
@@ -68,13 +63,15 @@ function _mint(recipient: Address, amount: u64): boolean {
  * @param amount - how much you want to increase the total supply
  * @returns true if the total supply has been increased
  */
-function _increaseTotalSupply(amount: u64): boolean {
+function _increaseTotalSupply(amount: u64): void {
   const oldTotalSupply = bytesToU64(totalSupply([]));
   const newTotalSupply = oldTotalSupply + amount;
-  // Overflow
-  if (oldTotalSupply > newTotalSupply) {
-    return false;
-  }
+
+  // Check overflow
+  assert(
+    oldTotalSupply < newTotalSupply,
+    'Requested mint amount causes an overflow',
+  );
+
   Storage.set(TOTAL_SUPPLY_KEY, u64ToBytes(newTotalSupply));
-  return true;
 }
