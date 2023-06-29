1dc396440dbf12000c700e0d3a59bc124924bfc9
Pierre Seznec <petterseznec@gmail.com>
2023-06-29 21:12:28 +0200

    upgrade as-types

--- smart-contracts/assembly/contracts/dns/__tests__/dns.spec.ts
+++ smart-contracts/assembly/contracts/dns/__tests__/dns.spec.ts
@@ -175,9 +175,7 @@ describe('DNS contract tests', () => {
     test('blacklist name not being admin', () => {
       switchUser(user1Addr);
       expect(() => {
-        const blacklistArgs = new Args()
-          .addNativeTypeArray(['blacklist1'])
-          .serialize();
+        const blacklistArgs = new Args().add(['blacklist1']).serialize();
         addWebsitesToBlackList(blacklistArgs);
       }).toThrow();
     });
@@ -185,18 +183,18 @@ describe('DNS contract tests', () => {
     test('add multiple websites to blacklist being dns admin', () => {
       switchUser(dnsAdmin);
       const websiteNames = ['flappy', 'example', 'website'];
-      const args = new Args().addNativeTypeArray(websiteNames);
+      const args = new Args().add(websiteNames);
       const websiteNamesBinary = args.serialize();
 
       // Clear existing blacklist (if any)
-      Storage.set(blackListKey, new Args().addNativeTypeArray([]).serialize());
+      Storage.set(blackListKey, new Args().add([] as string[]).serialize());
 
       // Call the addWebsitesToBlackList function with the list of website names
       addWebsitesToBlackList(websiteNamesBinary);
 
       // Retrieve the updated blacklist from storage
       const updatedBlacklist = new Args(getBlacklisted())
-        .nextNativeTypeArray<string>()
+        .nextStringArray()
         .unwrap();
 
       // Check if the website names have been added to the blacklist
@@ -208,7 +206,7 @@ describe('DNS contract tests', () => {
 
       // Retrieve the updated blacklist from storage
       const finalBlacklist = new Args(getBlacklisted())
-        .nextNativeTypeArray<string>()
+        .nextStringArray()
         .unwrap();
 
       // Check if we always get the same website names and not twice
@@ -237,7 +235,7 @@ describe('DNS contract tests', () => {
       switchUser(dnsAdmin);
       // Blacklist a list of names
       const blacklistNames = ['blacklist1', 'blacklist2', 'blacklist3'];
-      const blacklistArgs = new Args().addNativeTypeArray(blacklistNames);
+      const blacklistArgs = new Args().add(blacklistNames);
       const blacklistArgsBinary = blacklistArgs.serialize();
       addWebsitesToBlackList(blacklistArgsBinary);
 
