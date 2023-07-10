8c7bb480aefe37139827130bbf7e620cc51f49b5
Ben <br@massa.net>
2023-07-10 14:53:43 +0200

    update as config

--- smart-contracts/as-pect.config.js
+++ smart-contracts/as-pect.config.js
@@ -1,20 +1,28 @@
-import createMockedABI from './vm-mock/vm.js';
+import createMockedABI from '@massalabs/massa-as-sdk/vm-mock';
 
 export default {
-  // A set of globs passed to the glob package that qualify typescript files for testing.
-  entries: ['**/assembly/**/*.spec.ts'],
-
-  // A set of globs passed to the glob package that quality files to be added to each test.
-  include: ['assembly/**/*.include.ts'],
-
-  // A set of regexp that will disclude source files from testing.
+  /**
+   * A set of globs passed to the glob package that qualify typescript files for testing.
+   */
+  entries: ['assembly/**/__tests__/**/*.spec.ts'],
+  /**
+   * A set of globs passed to the glob package that quality files to be added to each test.
+   */
+  include: ['assembly/**/__tests__/**/*.include.ts'],
+  /**
+   * A set of regexp that will disclude source files from testing.
+   */
   disclude: [/node_modules/],
-
-  // Add your required AssemblyScript imports here.
+  /**
+   * Add your required AssemblyScript imports here.
+   */
   async instantiate(memory, createImports, instantiate, binary) {
     return createMockedABI(memory, createImports, instantiate, binary);
   },
-
-  // Specify if the binary wasm file should be written to the file system.
+  /** Enable code coverage. */
+  coverage: ['assembly/**/*.ts'],
+  /**
+   * Specify if the binary wasm file should be written to the file system.
+   */
   outputBinary: false,
 };
