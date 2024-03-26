9d2003a2e31897dbb768083754042c52484c5f3a
Grégory Libert <gl@massa.net>
2024-03-26 22:07:22 +0100

    add AccessControl object

---
+++ smart-contracts/assembly/access-control/__tests__/access_control.spec.ts
@@ -0,0 +1,127 @@
+import { Address, resetStorage } from "@massalabs/massa-as-sdk";
+import { AccessControl } from "../access_control";
+
+describe('AccessControl', () => {
+  test('should create new permissions', () => {
+    resetStorage();
+    const accessControl = new AccessControl<u8>(1);
+    const ADMIN = accessControl.newPermission('admin');
+    expect(ADMIN).toStrictEqual(1, 'Admin permission should be 2⁰ = 1');
+    const USER = accessControl.newPermission('user');
+    expect(USER).toStrictEqual(2, 'User permission should be 2¹ = 2');
+    const GUEST = accessControl.newPermission('guest');
+    expect(GUEST).toStrictEqual(4, 'Guest permission should be 2² = 4');
+  });
+
+  test('should panic on unknown permission', () => {
+    resetStorage();
+    expect(() => {
+      const accessControl = new AccessControl<u8>(1);
+      const userAddress = new Address('AU12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiKq');
+      accessControl.grantPermissionToUser(1, userAddress);
+    }).toThrow('permission does not exist');
+  });
+
+  test('should panic on adding to many permissions', () => {
+    resetStorage();
+    expect(() => {
+      const accessControl = new AccessControl<u8>(1);
+      accessControl.newPermission('p1');
+      accessControl.newPermission('p2');
+      accessControl.newPermission('p3');
+      accessControl.newPermission('p4');
+      accessControl.newPermission('p5');
+      accessControl.newPermission('p6');
+      accessControl.newPermission('p7');
+      accessControl.newPermission('p8');
+    }).toThrow('Permission index overflow');
+  });
+
+  test('should panic on adding permission twice', () => {
+    resetStorage();
+    expect(() => {
+      const accessControl = new AccessControl<u8>(1);
+      const ADMIN = accessControl.newPermission('admin');
+      const userAddress = new Address('AU12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiKq');
+      accessControl.grantPermissionToUser(ADMIN, userAddress);
+      accessControl.grantPermissionToUser(ADMIN, userAddress);
+    }).toThrow('User already has admin permission');
+  });
+
+  test('should panic on missing must have permission', () => {
+    resetStorage();
+    expect(() => {
+      const accessControl = new AccessControl<u8>(1);
+      const ADMIN = accessControl.newPermission('admin');
+      const USER = accessControl.newPermission('user');
+      const userAddress = new Address('AU12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiKq');
+      accessControl.grantPermissionToUser(USER, userAddress);
+      accessControl.mustHavePermission(ADMIN, userAddress);
+    }).toThrow('User does not have admin permission');
+  });
+
+  test('should add permissions to user', () => {
+    resetStorage();
+    const accessControl = new AccessControl<u8>(1);
+    const ADMIN = accessControl.newPermission('admin');
+    const USER = accessControl.newPermission('user');
+    const GUEST = accessControl.newPermission('guest');
+    const userAddress = new Address('AU12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiKq');
+    accessControl.grantPermissionToUser(USER, userAddress);
+    accessControl.grantPermissionToUser(GUEST, userAddress);
+
+    expect(accessControl.hasPermission(USER, userAddress)).toBeTruthy('User should have user permission');
+    expect(accessControl.hasPermission(GUEST, userAddress)).toBeTruthy('User should have guest permission');
+    expect(accessControl.hasPermission(ADMIN, userAddress)).toBeFalsy('User should not have admin permission');
+  });
+
+  test('should remove permissions from user', () => {
+    resetStorage();
+    const accessControl = new AccessControl<u8>(1);
+    const ADMIN = accessControl.newPermission('admin');
+    const USER = accessControl.newPermission('user');
+    const GUEST = accessControl.newPermission('guest');
+    const userAddress = new Address('AU12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiKq');
+    accessControl.grantPermissionToUser(USER, userAddress);
+    accessControl.grantPermissionToUser(GUEST, userAddress);
+    accessControl.grantPermissionToUser(ADMIN, userAddress);
+
+    expect(accessControl.hasPermission(USER, userAddress)).toBeTruthy('User should have user permission');
+    expect(accessControl.hasPermission(GUEST, userAddress)).toBeTruthy('User should have guest permission');
+    expect(accessControl.hasPermission(ADMIN, userAddress)).toBeTruthy('User should have admin permission');
+
+    accessControl.removePermissionFromUser(USER, userAddress);
+
+    expect(accessControl.hasPermission(USER, userAddress)).toBeFalsy('User should not have user permission');
+    expect(accessControl.hasPermission(GUEST, userAddress)).toBeTruthy('User should have guest permission');
+    expect(accessControl.hasPermission(ADMIN, userAddress)).toBeTruthy('User should have admin permission');
+  });
+
+  test('should return proper throw message', () => {
+    resetStorage();
+    const accessControl = new AccessControl<u8>(1);
+    const ADMIN = accessControl.newPermission('admin');
+    const USER = accessControl.newPermission('user');
+    const GUEST = accessControl.newPermission('guest');
+    const userAddress = new Address('AU12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiKq');
+
+    // accessControl.mustHavePermission(ADMIN, userAddress);
+    // accessControl.mustHavePermission(USER, userAddress);
+    // accessControl.mustHavePermission(GUEST, userAddress);
+  });
+
+  test('should handle multiple access control instances', () => {
+    resetStorage();
+    const accessControl1 = new AccessControl<u8>(1);
+    const ADMIN = accessControl1.newPermission('admin');
+
+    const accessControl2 = new AccessControl<u8>(2);
+    const MECANIC = accessControl2.newPermission('mecanic');
+
+    const userAddress = new Address('AU12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiKq');
+    accessControl1.grantPermissionToUser(ADMIN, userAddress);
+
+    expect(accessControl2.hasPermission(ADMIN, userAddress)).toBeFalsy('User should not have car mechanic permission');
+  });
+
+});
---
+++ smart-contracts/assembly/access-control/access_control.ts
@@ -0,0 +1,86 @@
+import { fromBytes, toBytes } from "@massalabs/as-types";
+import { Address, Storage } from "@massalabs/massa-as-sdk";
+
+/**
+ * Manages roles and permissions within a blockchain context using a bitmask approach.
+ * Each bit in a bitmask represents a distinct permission. This class exploits
+ * blockchain's native, map-like storage for efficient role and permission management.
+ * 
+ * Implementation details:
+ * - Permissions are encoded as bits in a bitmask for compact storage and easy manipulation.
+ * - User access rights are stored and managed in a similar bitmask format.
+ * - Utilizes blockchain's native storage, with ModuleId as a prefix to differentiate keys
+ *   belonging to different modules.
+ */
+export class AccessControl<T> {
+  // @ts-ignore non-number type
+  private nextPermissionIndex: T = 1;
+  private permissionsName: string[] = [];
+  private moduleId: u8;
+  private errPermissionDoesNotExist: string = 'Permission does not exist';
+
+  constructor(moduleId: u8) {
+    this.moduleId = moduleId;
+  }
+
+  private _getStorageKey(userAddress: Address): StaticArray<u8> {
+    const key = new StaticArray<u8>(1);
+    key[0] = this.moduleId;
+    return key.concat(userAddress.serialize());
+  }
+
+  private _getUserAccess(userAddress: Address): T {
+    const key = this._getStorageKey(userAddress);
+    return Storage.has(key) ? <T>fromBytes<T>(Storage.get(key)) : <T>0;
+  }
+
+  private _setUserAccess(userAddress: Address, access: T): void {
+    const key = this._getStorageKey(userAddress);
+    Storage.set(key, toBytes(access));
+  }
+
+  public newPermission(Permission: string): T {
+    this.permissionsName.push(Permission);
+    const r = this.nextPermissionIndex;
+    // @ts-ignore arithmetic operations on generic types
+    assert(this.nextPermissionIndex < (1 << ((<u8>sizeof<T>()) * 8 - 1)), 'Permission index overflow');
+    // @ts-ignore arithmetic operations on generic types
+    this.nextPermissionIndex <<= 1;
+    return r;
+  }
+
+  public grantPermissionToUser(permission: T, userAddress: Address): void {
+    assert(permission < this.nextPermissionIndex, this.errPermissionDoesNotExist);
+
+    const ua = this._getUserAccess(userAddress);
+    // @ts-ignore arithmetic operations on generic types
+    assert((ua & permission) != permission, `User already has '${this.permissionsName[permission/2]}' Permission`);
+    // @ts-ignore arithmetic operations on generic types
+    this._setUserAccess(userAddress, ua | permission);
+  }
+
+  public removePermissionFromUser(permission: T, userAddress: Address): void {
+    assert(permission < this.nextPermissionIndex, this.errPermissionDoesNotExist);
+    
+    const ua = this._getUserAccess(userAddress);
+    // @ts-ignore arithmetic operations on generic types
+    assert((ua & permission) == permission, `User does not have '${this.permissionsName[permission/2]}' Permission`);
+    // @ts-ignore arithmetic operations on generic types
+    this._setUserAccess(userAddress, ua & ~permission);
+  }
+
+  public hasPermission(permission: T, userAddress: Address): boolean {
+    assert(permission < this.nextPermissionIndex, this.errPermissionDoesNotExist);
+
+    const ua = this._getUserAccess(userAddress);
+    // @ts-ignore arithmetic operations on generic types
+    return (ua & permission) == permission;
+  }
+
+  public mustHavePermission(permission: T, userAddress: Address): void {
+    assert(
+      this.hasPermission(permission, userAddress),
+      `User does not have ${this.permissionsName[permission/2]} Permission`
+    );
+  }
+}
