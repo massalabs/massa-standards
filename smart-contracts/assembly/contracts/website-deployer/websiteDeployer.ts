6016542d348be5c7b261b3de9a7701e57d8005a2
Thomas Sénéchal <44696378+thomas-senechal@users.noreply.github.com>
2024-08-23 11:32:22 +0200

    Fix websiteDeployer.ts storage cost calculation (#169)
    * Fix websiteDeployer.ts storage cost calculation
    
    * Optimize a bit storage cost calculation in websiteDeployer.ts
    
    * Fix lint error in websiteDeployer.ts

--- smart-contracts/assembly/contracts/website-deployer/websiteDeployer.ts
+++ smart-contracts/assembly/contracts/website-deployer/websiteDeployer.ts
@@ -6,9 +6,12 @@ import {
   call,
   Context,
 } from '@massalabs/massa-as-sdk';
+import { OWNER_KEY } from '../utils/ownership-internal';
 
-const StorageCostPerByte = 1_000_000;
-const StorageKeyCreation = 10 * StorageCostPerByte;
+const baseCostBytes = 4;
+const StorageCostPerByte = 100_000; // 0.0001 MAS
+// baseCostBytes + CreationTimestampKeyLen as u8 + CreationTimestampValueLen as u64;
+const CreationTimestampStorageCost = 13;
 
 /**
  * Creates a new smart contract with the websiteDeployer.wasm file content.
@@ -20,13 +23,17 @@ export function main(_: StaticArray<u8>): void {
 
   const websiteAddr = createSC(bytes);
 
-  // this will be updated when charging storage key for actual size
-  // const StorageKeyCreation = stringToBytes(OWNER_KEY).length * StorageCostPerByte;
+  const OwnerStorageKeyLen = stringToBytes(OWNER_KEY).length;
+  const OwnerStorageValueLen = stringToBytes(
+    Context.caller().toString(),
+  ).length;
+
+  const OwnerStorageCost =
+    baseCostBytes + OwnerStorageKeyLen + OwnerStorageValueLen;
 
   // cost of storing owner key
   const coins =
-    StorageKeyCreation +
-    StorageCostPerByte * stringToBytes(Context.caller().toString()).length;
+    StorageCostPerByte * (OwnerStorageCost + CreationTimestampStorageCost);
 
   call(websiteAddr, 'constructor', new Args(), coins);
 
