5f4b94794acf21676b21bfa5982aadf35658291c
Peterjah <ps@massa.net>
2024-11-07 13:51:11 +0100

    update Readme

--- smart-contracts/assembly/contracts/index.ts
+++ smart-contracts/assembly/contracts/index.ts
@@ -1,7 +1,7 @@
 import * as FT from './FT';
 import * as NFT from './NFT';
 import * as ownership from './utils/ownership';
+import * as accessControl from './utils/accessControl';
+import * as MRC1155 from './MRC1155';
 
-export { FT };
-export { NFT };
-export { ownership };
+export { FT, NFT, ownership, accessControl, MRC1155 };
