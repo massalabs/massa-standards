6a275c7a19449edd49dff6a2fb7f0c61b4f62b6f
Peterjah <ps@massa.net>
2025-05-23 10:31:18 +0200

    add multicall execute contract

---
+++ smart-contracts/assembly/contracts/multicall/multicall.ts
@@ -0,0 +1,48 @@
+import { Args, i32ToBytes, stringToBytes } from '@massalabs/as-types';
+import {
+  getOpData,
+  hasOpKey,
+  env,
+  generateEvent,
+} from '@massalabs/massa-as-sdk';
+import { Call, CallResult } from './serializers/serializers';
+
+export function main(_: StaticArray<u8>): StaticArray<u8> {
+  const calls = deserializeCalls();
+  const results: CallResult[] = [];
+  for (let i = 0; i < calls.length; i++) {
+    const call = calls[i];
+    generateEvent(
+      `Calling ${call.contract} ${call.targetFunc} with ${call.coins} nMAS`,
+    );
+    const res = env.env.call(
+      call.contract,
+      call.targetFunc,
+      call.params,
+      call.coins,
+    );
+    results.push(new CallResult(res));
+  }
+  return new Args().addSerializableObjectArray(results).serialize();
+}
+
+const CALL_PREFIX = stringToBytes('C_');
+
+export function callKey(index: i32): StaticArray<u8> {
+  return CALL_PREFIX.concat(i32ToBytes(index));
+}
+
+function deserializeCalls(): Call[] {
+  const calls: Call[] = [];
+
+  let i = 0;
+  let key = callKey(i);
+  while (hasOpKey(key)) {
+    calls[i] = new Call();
+    calls[i].deserialize(getOpData(key), 0);
+    i++;
+    key = callKey(i);
+  }
+
+  return calls;
+}
