95dc36b13ece99d79e488ed7de01f98a8325f77e
Peterjah <ps@massa.net>
2025-05-27 23:39:19 +0200

    refactor accescontrol contract

--- smart-contracts/assembly/contracts/utils/accessControl-internal.ts
+++ smart-contracts/assembly/contracts/utils/accessControl-internal.ts
@@ -1,9 +1,52 @@
-import { Storage } from '@massalabs/massa-as-sdk';
+import {
+  Context,
+  createEvent,
+  generateEvent,
+  Storage,
+} from '@massalabs/massa-as-sdk';
 import { Args, stringToBytes } from '@massalabs/as-types';
+import { onlyOwner } from './ownership';
+import { _isOwner } from './ownership-internal';
 
 export const ROLES_KEY = '_ROLES';
 
 export const ROLE_GRANTED_EVENT = 'ROLE_GRANTED_EVENT';
+export const ROLE_REVOKED_EVENT = 'ROLE_REVOKED_EVENT';
+
+export function _grantRole(role: string, account: string): void {
+  onlyOwner();
+
+  assert(!_hasRole(role, account), `Account already has ${role} role`);
+
+  const membersList = _members(role);
+  membersList.push(account);
+  Storage.set(_roleKey(role), new Args().add(membersList).serialize());
+
+  generateEvent(createEvent(ROLE_GRANTED_EVENT, [role, account]));
+}
+
+export function _revokeRole(role: string, account: string): void {
+  const caller = Context.caller().toString();
+
+  if (!_isOwner(caller)) {
+    assert(
+      Context.caller().toString() === account,
+      `Caller is not ${account} or admin`,
+    );
+    assert(_hasRole(role, account), `Account does not have ${role} role`);
+  }
+
+  let membersList = _members(role);
+  if (membersList.length > 1) {
+    membersList[membersList.indexOf(account)] =
+      membersList[membersList.length - 1];
+    membersList.pop();
+  } else {
+    membersList = [];
+  }
+  Storage.set(_roleKey(role), new Args().add(membersList).serialize());
+  generateEvent(createEvent(ROLE_REVOKED_EVENT, [role, account]));
+}
 
 export function _roleKey(role: string): StaticArray<u8> {
   return stringToBytes(ROLES_KEY + role);
@@ -19,4 +62,11 @@ export function _members(role: string): string[] {
 
 export function _hasRole(role: string, account: string): bool {
   return _members(role).includes(account);
+}
+
+export function _onlyRole(role: string): void {
+  assert(
+    _hasRole(role, Context.caller().toString()),
+    `Caller does not have ${role} role`,
+  );
 }
--- smart-contracts/assembly/contracts/utils/accessControl.ts
+++ smart-contracts/assembly/contracts/utils/accessControl.ts
@@ -1,17 +1,12 @@
-import {
-  generateEvent,
-  Storage,
-  createEvent,
-  Context,
-} from '@massalabs/massa-as-sdk';
 import { Args, boolToByte } from '@massalabs/as-types';
 import { onlyOwner } from './ownership';
-import { _hasRole, _members, _roleKey } from './accessControl-internal';
-import { _isOwner } from './ownership-internal';
-
-export const ROLES_KEY = '_ROLES';
-
-export const ROLE_GRANTED_EVENT = 'ROLE_GRANTED_EVENT';
+import {
+  _grantRole,
+  _hasRole,
+  _members,
+  _onlyRole,
+  _revokeRole,
+} from './accessControl-internal';
 
 /**
  *  Set the role for account
@@ -27,13 +22,7 @@ export function grantRole(binaryArgs: StaticArray<u8>): void {
     .nextString()
     .expect('account argument is missing or invalid');
 
-  assert(!_hasRole(role, account), `Account already has ${role} role`);
-
-  const membersList = _members(role);
-  membersList.push(account);
-  Storage.set(_roleKey(role), new Args().add(membersList).serialize());
-
-  generateEvent(createEvent(ROLE_GRANTED_EVENT, [role, account]));
+  _grantRole(role, account);
 }
 
 /**
@@ -71,34 +60,14 @@ export function hasRole(binaryArgs: StaticArray<u8>): StaticArray<u8> {
  * @param role - role name string
  * @param account - account address string
  */
-export function revokeRole(binaryArgs: StaticArray<u8>): StaticArray<u8> {
+export function revokeRole(binaryArgs: StaticArray<u8>): void {
   const args = new Args(binaryArgs);
   const role = args.nextString().expect('role argument is missing or invalid');
   const account = args
     .nextString()
     .expect('account argument is missing or invalid');
 
-  const caller = Context.caller().toString();
-
-  if (!_isOwner(caller)) {
-    assert(
-      Context.caller().toString() === account,
-      `Caller is not ${account} or admin`,
-    );
-    assert(_hasRole(role, account), `Account does not have ${role} role`);
-  }
-
-  let membersList = _members(role);
-  if (membersList.length > 1) {
-    membersList[membersList.indexOf(account)] =
-      membersList[membersList.length - 1];
-    membersList.pop();
-  } else {
-    membersList = [];
-  }
-  Storage.set(_roleKey(role), new Args().add(membersList).serialize());
-
-  return [];
+  _revokeRole(role, account);
 }
 
 /**
@@ -111,8 +80,5 @@ export function onlyRole(binaryArgs: StaticArray<u8>): void {
   const role = new Args(binaryArgs)
     .nextString()
     .expect('role argument is missing or invalid');
-  assert(
-    _hasRole(role, Context.caller().toString()),
-    `Caller does not have ${role} role`,
-  );
+  _onlyRole(role);
 }
